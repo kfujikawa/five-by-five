b175fef7c7d7f593458d67219afdfd3d96083b60
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-06-16 10:51:00 -0700

    Update tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,14 +1,13 @@
 const mocha = require('mocha');
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const faker = require('faker');
-const jwt = require('express-jwt');
 
-const should = chai.should();
 const expect = chai.expect;
 
 const Goal = require('../api/goal/goal.model');
+const User = require('../api/user/user.model');
 const { app, runServer, closeServer } = require('../server');
 const { TEST_DATABASE_URL } = require('../config/config');
 
@@ -17,19 +16,31 @@ chai.use(chaiHttp);
 function generateGoalData() {
   return {
     name: faker.random.words(),
-    type: "career",
+    type: 'career',
     isChecked: faker.random.boolean()
   };
 }
 
+function authenticate(callback) {
+  chai
+    .request(app)
+    .post('/auth/login')
+    .send({ email: 'test', password: 'test' })
+    .then(function(res) {
+      console.log('trying to log in');
+      callback(res.body.token);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
 function seedGoalData() {
-  console.info('Seeding goal data');
   const seedData = [];
 
   for (let i = 1; i < 3; i++) {
     seedData.push(generateGoalData());
   }
-  console.log(seedData);
 
   return Goal.create(seedData);
 }
@@ -39,49 +50,47 @@ function tearDownDb() {
   return mongoose.connection.dropDatabase();
 }
 
-describe("API Tests for Goals", function () {
+describe('API Tests for Goals', function() {
+  let token = '';
   before(function() {
+    User.create({ email: 'test', password: 'test', name: 'test' });
     return runServer(TEST_DATABASE_URL);
   });
 
-  beforeEach(function() {
-    return seedGoalData();
-  });
-
-  afterEach(function() {
-    return tearDownDb();
-  });
-
   after(function() {
+    tearDownDb();
+
     return closeServer();
   });
 
-  it('should create a goal', function() {
-    const newGoal = generateGoalData();
-    const token = jwt({ secret: 'secret' });
-
-    console.log(token);
-
-    return chai.request(app).post('/api/goals')
-      .send(newGoal)
-      .then(function(res) {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.should.be.a('Object');
-        res.body.name.should.equal(newGoal.name);
-        console.log(newGoal.name);
-        res.body.name.should.be.a('String');
-        expect(res.body.name.length).to.be.at.least(1);
-        expect(res.body.name.length).to.be.below(40);
+  it('should create a goal', function(done) {
+    authenticate(token => {
+      const newGoal = generateGoalData();
+      return chai
+        .request(app)
+        .post('/api/goals')
+        .set('Authorization', 'bearer ' + token)
+        .send(newGoal)
+        .then(function(res) {
+          expect(res.status).to.equal(200);
+          expect(res.body[0].name).to.equal(newGoal.name);
+          done();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
     });
   });
 
-  it("should return a 200 status code and HTML on GET", function () {
-    return chai.request(app)
-      .get("/")
-      .then(function(res){
-        res.should.have.status(200);
-        res.should.be.html;
-    });
+  it('should return a 200 status code and HTML on GET', function() {
+    return chai
+      .request(app)
+      .get('/')
+      .then(function(res) {
+        expect(res.status).to.equal(200);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   });
 });
