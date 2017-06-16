052503449ef61b47928bbcd5fa3a209948589cce
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-06-16 12:04:15 -0700

    update goals

--- test/test-server.js
+++ test/test-server.js
@@ -21,6 +21,14 @@ function generateGoalData() {
   };
 }
 
+function generateDefaultGoalData() {
+  return {
+    name: faker.random.words(),
+    type: 'career',
+    isChecked: false
+  };
+}
+
 function authenticate(callback) {
   chai
     .request(app)
@@ -82,10 +90,68 @@ describe('API Tests for Goals', function() {
     });
   });
 
+  it('should return all goals', function(done) {
+    authenticate(token => {
+      return chai
+        .request(app)
+        .get('/api/goals')
+        .set('Authorization', 'bearer ' + token)
+        .then(function(res) {
+          expect(res.status).to.be.a.object;
+          done();
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
+  });
+
+  // it('should delete a goal', function(done) {
+  //   authenticate(token => {
+  //     const newGoal = generateGoalData();
+  //     return chai
+  //       .request(app)
+  //       .post('/api/goals')
+  //       .set('Authorization', 'bearer ' + token)
+  //       .send(newGoal)
+  //       .then(function(res) {
+  //         expect(res.status).to.equal(200);
+  //         expect(res.body[0].name).to.equal(newGoal.name);
+  //         done();
+  //       })
+  //       .catch(function(error) {
+  //         console.log(error);
+  //       });
+  //   });
+  // });
+
+  // it('should update the status of a goal by id', function(done) {
+  //   authenticate(token => {
+  //     const goal = res.body[0];
+  //     const id = res.body[0].id;
+
+  //     return chai
+  //       .request(app)
+  //       .set('Authorization', 'bearer ' + token)
+  //       .get('/api/goals')
+  //       .put('/api/goals/' + id)
+  //       .send(goal.isChecked(true))
+  //       .then(function(res) {
+  //         expect(res.status).to.equal(201);
+  //         expect(res.body[0].isChecked).to.equal(true);
+  //         done();
+  //       })
+  //       .catch(function(error) {
+  //         console.log(error);
+  //       });
+  //   });
+  // });
+
   it('should return a 200 status code and HTML on GET', function() {
     return chai
       .request(app)
       .get('/')
+      .set('Authorization', 'bearer ' + token)
       .then(function(res) {
         expect(res.status).to.equal(200);
       })
